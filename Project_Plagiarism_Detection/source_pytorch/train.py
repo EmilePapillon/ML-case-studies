368956df7ac677f23e169b2b349993a87ceb6aa0
Emile Papillon <epapillon@live.ca>
2021-05-05 20:50:20 +0000

    Added and trained a pytorch model

--- Project_Plagiarism_Detection/source_pytorch/train.py
+++ Project_Plagiarism_Detection/source_pytorch/train.py
@@ -1,16 +1,22 @@
+from __future__ import print_function # future proof
 import argparse
-import json
+import sys
 import os
+import json
+
 import pandas as pd
+
+# pytorch
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data
 
-# imports the model in model.py by name
-from model import BinaryClassifier
+# import model
+from model import SimpleNet
+
 
 def model_fn(model_dir):
-    """Load the PyTorch model from the `model_dir` directory."""
     print("Loading model.")
 
     # First, load the parameters used to create the model.
@@ -23,77 +29,92 @@ def model_fn(model_dir):
 
     # Determine the device and construct the model.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = BinaryClassifier(model_info['input_features'], model_info['hidden_dim'], model_info['output_dim'])
+    model = SimpleNet(model_info['input_dim'], 
+                      model_info['hidden_dim'], 
+                      model_info['output_dim'])
 
     # Load the stored model parameters.
     model_path = os.path.join(model_dir, 'model.pth')
     with open(model_path, 'rb') as f:
         model.load_state_dict(torch.load(f))
+    
+    return model.to(device)
 
-    # set to eval mode, could use no_grad
-    model.to(device).eval()
 
-    print("Done loading model.")
-    return model
+# Load the training data from a csv file
+def _get_train_loader(batch_size, data_dir):
+    print("Get data loader.")
 
-# Gets training data in batches from the train.csv file
-def _get_train_data_loader(batch_size, training_dir):
-    print("Get train data loader.")
+    # read in csv file
+    train_data = pd.read_csv(os.path.join(data_dir, "train.csv"), header=None, names=None)
 
-    train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
-
+    # labels are first column
     train_y = torch.from_numpy(train_data[[0]].values).float().squeeze()
+    # features are the rest
     train_x = torch.from_numpy(train_data.drop([0], axis=1).values).float()
 
+    # create dataset
     train_ds = torch.utils.data.TensorDataset(train_x, train_y)
 
     return torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
 
 
-# Provided training function
-def train(model, train_loader, epochs, criterion, optimizer, device):
+# Provided train function
+def train(model, train_loader, epochs, optimizer, criterion, device):
     """
     This is the training method that is called by the PyTorch training script. The parameters
     passed are as follows:
     model        - The PyTorch model that we wish to train.
     train_loader - The PyTorch DataLoader that should be used during training.
     epochs       - The total number of epochs to train for.
-    criterion    - The loss function used for training. 
     optimizer    - The optimizer to use during training.
+    criterion    - The loss function used for training. 
     device       - Where the model and data should be loaded (gpu or cpu).
     """
     
-    # training loop is provided
     for epoch in range(1, epochs + 1):
-        model.train() # Make sure that the model is in training mode.
-
+        model.train()
         total_loss = 0
-
-        for batch in train_loader:
-            # get data
-            batch_x, batch_y = batch
-
-            batch_x = batch_x.to(device)
-            batch_y = batch_y.to(device)
-
-            optimizer.zero_grad()
-
-            # get predictions from model
-            y_pred = model(batch_x)
-            
-            # perform backprop
-            loss = criterion(y_pred, batch_y)
+        for batch_idx, (data, target) in enumerate(train_loader, 1):
+            # prep data
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad() # zero accumulated gradients
+            # get output of SimpleNet
+            output = model(data)
+            # calculate loss and perform backprop
+            loss = criterion(output, target)
             loss.backward()
             optimizer.step()
-            
-            total_loss += loss.data.item()
-
+    
+            total_loss += loss.item()
+        
+        # print loss stats
         print("Epoch: {}, Loss: {}".format(epoch, total_loss / len(train_loader)))
+
+    # save after all epochs
+    save_model(model, args.model_dir)
+
+
+# Provided model saving functions
+def save_model(model, model_dir):
+    print("Saving the model.")
+    path = os.path.join(model_dir, 'model.pth')
+    # save state dictionary
+    torch.save(model.cpu().state_dict(), path)
+    
+def save_model_params(model, model_dir):
+    model_info_path = os.path.join(args.model_dir, 'model_info.pth')
+    with open(model_info_path, 'wb') as f:
+        model_info = {
+            'input_dim': args.input_dim,
+            'hidden_dim': args.hidden_dim,
+            'output_dim': args.output_dim
+        }
+        torch.save(model_info, f)
 
 
 ## TODO: Complete the main code
 if __name__ == '__main__':
-    
     # All of the model parameters and training parameters are sent as arguments
     # when this script is executed, during a training job
     
@@ -102,63 +123,54 @@ if __name__ == '__main__':
 
     # SageMaker parameters, like the directories for training data and saving models; set automatically
     # Do not need to change
-    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    parser.add_argument('--hosts', type=list, default=json.loads(os.environ['SM_HOSTS']))
+    parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
     
     # Training Parameters, given
-    parser.add_argument('--batch-size', type=int, default=10, metavar='N',
-                        help='input batch size for training (default: 10)')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
     parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
+  
+    ## TODO: Add args for the three model parameters: input_dim, hidden_dim, output_dim
+    # Model parameters
+    parser.add_argument('--input_dim', type=int, default=2, metavar='I')
+    parser.add_argument('--hidden_dim', type=int, default=8, metavar='H')
+    parser.add_argument('--output_dim', type=int, default=1, metavar='O')
     
-    ## TODO: Add args for the three model parameters: input_features, hidden_dim, output_dim
-    # Model Parameters
-    
-    
-    # args holds all passed-in arguments
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Using device {}.".format(device))
-
+    
+    # set the seed for generating random numbers
     torch.manual_seed(args.seed)
-
-    # Load the training data.
-    train_loader = _get_train_data_loader(args.batch_size, args.data_dir)
-
-
-    ## --- Your code here --- ##
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
+        
+    # get train loader
+    train_loader = _get_train_loader(args.batch_size, args.data_dir) # data_dir from above..
+    
     
     ## TODO:  Build the model by passing in the input params
     # To get params from the parser, call args.argument_name, ex. args.epochs or ards.hidden_dim
     # Don't forget to move your model .to(device) to move to GPU , if appropriate
-    model = None
+    model = SimpleNet(args.input_dim, args.hidden_dim, args.output_dim)
+    
+    # Given: save the parameters used to construct the model
+    save_model_params(model, args.model_dir)
 
     ## TODO: Define an optimizer and loss function for training
-    optimizer = None
-    criterion = None
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+    criterion = nn.BCELoss()
 
-    # Trains the model (given line of code, which calls the above training function)
-    train(model, train_loader, args.epochs, criterion, optimizer, device)
-
-    ## TODO: complete in the model_info by adding three argument names, the first is given
-    # Keep the keys of this dictionary as they are 
-    model_info_path = os.path.join(args.model_dir, 'model_info.pth')
-    with open(model_info_path, 'wb') as f:
-        model_info = {
-            'input_features': args.input_features,
-            'hidden_dim': <add_arg>,
-            'output_dim': <add_arg>,
-        }
-        torch.save(model_info, f)
-        
-    ## --- End of your code  --- ##
     
-
-	# Save the model parameters
-    model_path = os.path.join(args.model_dir, 'model.pth')
-    with open(model_path, 'wb') as f:
-        torch.save(model.cpu().state_dict(), f)
+    # Trains the model (given line of code, which calls the above training function)
+    # This function *also* saves the model state dictionary
+    train(model, train_loader, args.epochs, optimizer, criterion, device)
+    
